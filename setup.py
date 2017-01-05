e7b25ef639ba4efdd9cf1f62e4642ca3efb32c94
Ilja Bauer <iljabauer@gmail.com>
2017-01-05 16:47:04 +0100

    Increased version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='dj-async-mailbackend',
-    version='1.1.0',
+    version='1.2.0',
     packages=['async_mails', 'async_mails.migrations'],
     url='http://polylith.de/',
     license='MIT',
